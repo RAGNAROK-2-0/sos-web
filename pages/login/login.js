6d07f5a801ccf2552c180f479e201998566b3e31
jefferson.matos <jefferson.matos@anchieta.br>
2021-11-19 10:31:57 -0300

    alter: login header api

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,31 +1,23 @@
 $(document).ready(function () {
-    document.getElementById('submitLoginForm').addEventListener(
-        'click', stopDefAction, false
-    );
+  document
+    .getElementById("submitLoginForm")
+    .addEventListener("click", stopDefAction, false);
 });
 
-
 function stopDefAction(evt) {
-    evt.preventDefault();
+  evt.preventDefault();
 }
 
 function authUser() {
-    $.ajax({
-        type: "POST",
-        url: "https://sos-help-desq.herokuapp.com/auth",
-        data: {
-            num_func: $("#email").val(),
-            senha: $("#password").val(),
-        },
-        dataType: "json",
-        success: function (json) {
-            console.log(json);
-        }, error: function (json) {
-            console.log(json);
+  const url = "https://sos-help-desq.herokuapp.com/auth";
+  const body = { email: $("#email").val(), senha: $("#password").val() };
 
-        }
-    });
-
+  fetch(url, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(body)
+  });
 }
-
-
