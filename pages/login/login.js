025a9e07488f95a8333300cfea82919b1db25160
jeffsLM <jeffes.dev@gmail.com>
2021-11-20 14:23:29 -0300

    feat: loggin page  - persist Login

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,23 +1,54 @@
 $(document).ready(function () {
-  document
-    .getElementById("submitLoginForm")
-    .addEventListener("click", stopDefAction, false);
+    document
+        .getElementById("submitLoginForm")
+        .addEventListener("click", stopDefAction, false);
 });
 
 function stopDefAction(evt) {
-  evt.preventDefault();
+    evt.preventDefault();
 }
 
 function authUser() {
-  const url = "https://sos-help-desq.herokuapp.com/auth";
-  const body = { email: $("#email").val(), senha: $("#password").val() };
+    const url = "https://sos-help-desq.herokuapp.com/auth";
+    const body = { email: $("#email").val(), senha: $("#password").val() };
+    $("#loading").addClass("fa fa-circle-o-notch fa-spin")
+    fetch(url, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    }).then(response => {
+        if (response.status !== 200) {
 
-  fetch(url, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(body)
-  });
+            throw new Error(response.status)
+        }
+        return response.json();
+    }).then(userDataSquema => {
+
+        var uma_semana = new Date();
+        uma_semana.setDate(uma_semana.getDate() + 7);
+        let data = uma_semana.toGMTString();
+
+        
+        setCookie('x-sos-auth-email', userDataSquema.email, data)
+        setCookie('x-sos-auth-nome', userDataSquema.nome, data)
+        setCookie('x-sos-auth-num_func', userDataSquema.num_func, data)
+        setCookie('x-sos-auth-setor', userDataSquema.setor, data)
+        setCookie('x-sos-auth-criado',new Intl.DateTimeFormat('pt-BR').format(new Date(userDataSquema.data_insercao)), data)
+
+        !!getCookie('x-sos-auth-callbackPage') ? navigateToPage(getCookie('x-sos-auth-callbackPage')) : navigateToPage('')
+
+
+    }).catch(err => {
+        $("#loading").removeClass("fa fa-circle-o-notch fa-spin")
+        $("#email").css("border", "1px solid red")
+        $("#password").css("border", "1px solid red")
+        $("#idLabelPassword").text("Usuário ou Senha inválidas!")
+    });
 }
+
+
+
+
