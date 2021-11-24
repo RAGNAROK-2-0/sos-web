0bd7c9966942027450364dc803f7779765f5c321
Jbaldui <61895428+Jbaldui@users.noreply.github.com>
2021-11-24 20:49:00 -0300

    Template

---
+++ pages/template copy/template.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+
+    //assim que for autenticado o usuario deve retornar a pagina  '/pages/template/template.html'
+    verificaUsuarioAutenticado('/pages/template/template.html');
+});
